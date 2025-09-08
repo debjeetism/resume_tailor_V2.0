3167d76fbb7e1bcc8464c66a926a96531bcde300
debjeetism <debjeet71@gmail.com>
2025-09-09 03:02:27 +0530

    api

--- resume-tailor-frontend/src/api/resume.js
+++ resume-tailor-frontend/src/api/resume.js
@@ -1,6 +1,8 @@
  import axios from "axios";
 
-const API = axios.create({ baseURL: "http://127.0.0.1:8010" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8010",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
